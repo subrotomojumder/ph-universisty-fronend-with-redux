801804fc1b296cac8f95a10d3355b3a6f4b0660c
Janardhan Majumder <subrotomojumder03@gmail.com>
2024-02-21 19:49:14 +0600

    last commit

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -18,10 +18,6 @@ type TFormProps = {
   //     password: string;
   //   };
 } & TFormConfig;
-//  defaultValues?: {
-//     id: string;
-//     password: string;
-//   };
 const PHForm = ({ onSubmit, defaultValues, children }: TFormProps) => {
   const formConfig: TFormConfig = {};
 
